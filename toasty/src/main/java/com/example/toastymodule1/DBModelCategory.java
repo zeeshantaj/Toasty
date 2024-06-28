3c87f2b749b96a35800ac107ac3eec838140986d
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-06-28 18:56:55 +0500

    db model commit

--- toasty/src/main/java/com/example/toastymodule1/DBModelCategory.java
+++ toasty/src/main/java/com/example/toastymodule1/DBModelCategory.java
@@ -1,4 +1,11 @@
 package com.example.toastymodule1;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class DBModelCategory {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
