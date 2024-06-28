30174f7e7f5f5a8f0631452a04333abac6059d2d
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-06-28 13:35:46 +0500

    package refactor commit

--- app/src/main/java/com/example/toasty2/MainActivity.java
+++ app/src/main/java/com/example/toasty2/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.toasty.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
