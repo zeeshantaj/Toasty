c46fd6d54bd9ce6692e995554194769d6c0ab161
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-07-01 14:02:46 +0500

    java version11

--- toasty/src/main/java/com/example/toastymodule1/CommonUtilsMethods.java
+++ toasty/src/main/java/com/example/toastymodule1/CommonUtilsMethods.java
@@ -1,10 +1,13 @@
 package com.example.toastymodule1;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.widget.Toast;
 
 public class CommonUtilsMethods {
     public static void commonToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
     }
+
 }
